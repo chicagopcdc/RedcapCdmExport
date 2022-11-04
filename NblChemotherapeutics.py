cf5b47f84ca322fa000a0265170a1a09fa7a8397
Brian Furner <bfurner@bsd.uchicago.edu>
2022-11-04 16:46:13 -0500

    normalizing labels to uppercase

--- NblChemotherapeutics.py
+++ NblChemotherapeutics.py
@@ -6,14 +6,17 @@ import ExportSettings as es
 ### list of fields to export from REDCap CDM
 fields_to_export=['record_id','dob','medication_label','medication_date','medication_dosage','medication_status','cog_id']
 ### list of medication names to export from REDCap CDM
-meds_list = es.meds_list
+meds_list = list(map(lambda x: x.upper(), es.meds_list))
 ### list of medication statuses to export from REDCap CDM
-statuses_to_export=es.statuses_to_export
+statuses_to_export = list(map(lambda x: x.upper(), es.statuses_to_export))
 
 ### set up REDCap connection and export data to a Pandas dataframe
 project = Project(es.api_url, es.api_key)
 data = project.export_records(format_type="df", fields=fields_to_export)
 df = pd.DataFrame(data=data)
+### convert medication labels and statuses to all uppercase
+df['medication_label'] = df['medication_label'].apply(lambda x: str(x).upper())
+df['medication_status'] = df['medication_status'].apply(lambda x: str(x).upper())
 
 ### create dataframe with only the records that have date of birth
 df_dob=df[~df['dob'].isnull()]['dob']
@@ -34,8 +37,8 @@ df_out['cog_id'] = df_out['cog_id'].astype(int)
 ### calculate age in days at time of medication
 df_out['age_at_medication'] = pd.to_datetime(df_out['medication_date'])-pd.to_datetime(df_out['dob'])
 ### drop columns that are not needed and reorder columns
-df_out = df_out.drop(columns=['dob','medication_date','redcap_repeat_instance','redcap_repeat_instrument']).sort_values(['record_id','age_at_medication'])
-cols = ['cog_id','medication_label','medication_dosage','medication_status','age_at_medication']
+df_out = df_out.drop(columns=['dob','medication_date','medication_dosage','redcap_repeat_instance','redcap_repeat_instrument']).sort_values(['record_id','age_at_medication'])
+cols = ['cog_id','medication_label','medication_status','age_at_medication']
 df_out = df_out[cols]
 ### output to csv
 df_out.to_csv(es.csv_out)
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ PyCap is required to run this export.  To install PyCap, execute the following:
 
 Local settings need to be specified in the `ExportSettings.py` file before running the program.
 
+The code expects a REDCap CDM project (configured to extract DOB, and medications) with an additional `identity_mapping` instrument containing a single `text` field named `cog_id` that holds the COG Identifier of the participant.  This field will be used when creating the CSV export rather than the medical record number.
+
 Once settings have been specified, one can execute the export by running the following:
 
     python NblChemotherapeutics.py
